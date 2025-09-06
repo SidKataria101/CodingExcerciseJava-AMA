6cde3f7ed9d08ee67170654ac9ba88300bbb18f4
Sid Kataria <sideekataria@gmail.com>
2025-09-06 15:42:28 -0600

    Minor changes

--- ShiftString/src/Validator.java
+++ ShiftString/src/Validator.java
@@ -9,7 +9,7 @@ public class Validator {
     private static final Pattern VALID_STRING_PATTERN = Pattern.compile("[a-zA-Z\\s]+");
     public static final String SHIFT_RANGE_MESSAGE = "Shift positions must be between " + MIN_SHIFT + " and " + MAX_SHIFT;
 
-        /**
+    /**
      * Validates the shift positions.
      * @param shiftPositions the number of positions to validate
      * @throws IllegalArgumentException if the input is invalid
@@ -17,7 +17,6 @@ public class Validator {
     static int validateInt(int shiftPositions) {
         // Input validation for shift positions
         if (shiftPositions < MIN_SHIFT || shiftPositions > MAX_SHIFT) {
-            ConsoleUI.logError("Illegal argument in validateInt", new IllegalArgumentException("Shift positions must be between -1000 and 1000"));
             throw new IllegalArgumentException("Shift positions must be between -1000 and 1000");
         } 
         return shiftPositions;
@@ -31,13 +30,10 @@ public class Validator {
     static String validateString(String inputString) {
         // Input validation
         if (inputString == null || inputString.isEmpty()) {
-            ConsoleUI.logError(inputString, new IllegalArgumentException("Input string cannot be null or empty"));
             throw new IllegalArgumentException("Input string cannot be null or empty");
         } else if (!VALID_STRING_PATTERN.matcher(inputString).matches()) {
-            ConsoleUI.logError(inputString, new IllegalArgumentException("Input string must contain only alphabetic letters (a-z, A-Z) and spaces"));
             throw new IllegalArgumentException("Input string must contain only alphabetic letters (a-z, A-Z) and spaces");
         } else if (inputString.length() > MAX_STRING_LENGTH) {
-            ConsoleUI.logError(inputString, new IllegalArgumentException("Input string must not exceed 1000 characters"));
             throw new IllegalArgumentException("Input string must not exceed 1000 characters");
         }
         return inputString;
